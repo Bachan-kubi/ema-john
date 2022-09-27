04d0dc03893d1d3e87b5276d7dff34eade9dda7b
Bachan-kubi <felixghagra@gmail.com>
2022-09-27 20:54:32 +0600

    selected food desing changed

--- src/components/SelectedMeals/SelectedMeals.js
+++ src/components/SelectedMeals/SelectedMeals.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './SelectedMeals.css'
 
 const SelectedMeals = (props) => {
     return (
-        <div className='selected-meal'>
-            <h5>Selected Meals: <h2>{props.selected}</h2></h5>
+        <div className='selected-meal' >
+            <h3>Selected Meals</h3>
+            <hr />
+            <h4>{props.selected}</h4>
         </div>
     );
 };
